9b1782e923fc4cea2e8b511d6191c7fc0e5460f9
Colin Brown <colinbrownec@gmail.com>
2014-11-24 15:23:53 -0600

    MIT compatible

--- api/optimize.c
+++ api/optimize.c
@@ -44,8 +44,6 @@ static int my_isnan(double x) { return x != x; }
 
 #include "cdirect.h"
 
-#include "luksan.h"
-
 #include "crs.h"
 
 #include "mlsl.h"
@@ -533,25 +531,6 @@ static nlopt_result nlopt_optimize_(nlopt_opt opt, double *x, double *minf)
 	      return praxis_(0.0, DBL_EPSILON, 
 			     step, ni, x, f_bound, opt, &stop, minf);
 	 }
-
-	 case NLOPT_LD_LBFGS: 
-	      return luksan_plis(ni, f, f_data, lb, ub, x, minf, 
-				 &stop, opt->vector_storage);
-
-	 case NLOPT_LD_VAR1: 
-	 case NLOPT_LD_VAR2: 
-	      return luksan_plip(ni, f, f_data, lb, ub, x, minf, 
-				 &stop, opt->vector_storage,
-				 algorithm == NLOPT_LD_VAR1 ? 1 : 2);
-
-	 case NLOPT_LD_TNEWTON: 
-	 case NLOPT_LD_TNEWTON_RESTART: 
-	 case NLOPT_LD_TNEWTON_PRECOND: 
-	 case NLOPT_LD_TNEWTON_PRECOND_RESTART: 
-	      return luksan_pnet(ni, f, f_data, lb, ub, x, minf,
-				 &stop, opt->vector_storage,
-				 1 + (algorithm - NLOPT_LD_TNEWTON) % 2,
-				 1 + (algorithm - NLOPT_LD_TNEWTON) / 2);
 
 	 case NLOPT_GN_CRS2_LM:
 	      if (!finite_domain(n, lb, ub)) return NLOPT_INVALID_ARGS;
